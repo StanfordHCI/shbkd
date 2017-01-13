6ee99ab58f639f8d7a2f6b9931a316e5ae72e3d2
govindad <govinda@govindadasu.com>
2017-01-12 23:24:41 -0800

    static root back, allowed hosts

--- taskproj/settings.py
+++ taskproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5jzgtl*bz$qx5txa_v%!hl#eaye$j#64os@23)d^%gm2a@rpvy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://pptbkd-env.us-west-2.elasticbeanstalk.com/']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
